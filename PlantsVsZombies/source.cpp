75aa19b58c1951c866d64bcd220201bcb2bb52f7
Adnan <adnanfreelancer75500@gmail.com>
2024-05-09 18:51:19 +0500

    Screens and text

--- PlantsVsZombies/source.cpp
+++ PlantsVsZombies/source.cpp
@@ -1,4 +1,5 @@
 ﻿#include <SFML/Window/Cursor.hpp>
+#include <SFML/Graphics/Text.hpp>
 #include <SFML/Graphics.hpp>
 #include <iostream>
 #include <ctime>
@@ -11,12 +12,18 @@
 #include "LawnMowerFactory.h"
 using namespace sf;
 using namespace std;
-
+void renderHome(int,int,RenderWindow&);
+void renderInstructions(int,int,RenderWindow&);
+void renderLeaderboard(int, int, RenderWindow&);
 void renderGame(
     int GAME_WIDTH,
 	int GAME_HEIGHT,
 	RenderWindow &window
 ) {
+    Font font;
+    if (!font.loadFromFile("new.ttf")) {
+        cout << "ERROR LOADING FONT" << endl;
+    }
     Texture map;
     map.loadFromFile("./Images/background.png");
     map.setRepeated(true);
@@ -48,7 +55,12 @@ void renderGame(
     GameCursor cursor;
     Shop sh;
     int currency = 0;
-
+    Text currencyText;
+    currencyText.setFont(font); // Set the font
+    currencyText.setCharacterSize(24); // Set the character size
+    currencyText.setFillColor(Color::Black); // Set the color
+    currencyText.setString(to_string(currency)); // Set the content
+    currencyText.setPosition(40, 70); // Set the position on the screen
     PlantFactory plantFactory;
     ZombieFactory zombieFactory(20);
     SunFactory sunFactory(20);
@@ -154,7 +166,7 @@ void renderGame(
                 cursor.renderCursor(clickPosition, currency);
             }
         }
-
+        currencyText.setString(to_string(currency));
         for (int i = 0; i < lawnMowerFactory.lawnmowers_created; i++)
         {
             if (lawnMowerFactory.lawnmowers[i]->exist)
@@ -362,11 +374,115 @@ void renderGame(
                 }
             }
         }
+        window.draw(currencyText);
         window.display();
     }
     return;
 }
 
+void renderHome(
+    int GAME_WIDTH,
+    int GAME_HEIGHT,
+    RenderWindow& window
+) {
+    Texture HomeTexture;
+    HomeTexture.loadFromFile("./Images/homescreen.png");
+    Sprite home;
+    home.setTexture(HomeTexture);
+    home.setPosition(0, 0);
+    while (window.isOpen()) {
+        Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == Event::Closed) {
+                window.close();
+            }
+            else if (event.type == Event::MouseButtonPressed && event.mouseButton.button == Mouse::Left) {
+                int mouseX = event.mouseButton.x;
+                int mouseY = event.mouseButton.y;
+
+                // Check if mouse click falls within the specified area
+                if (mouseX >= 66 && mouseX <= 635 && mouseY >= 356 && mouseY <= 501) {
+                    // Call renderGame function
+                    renderGame(GAME_WIDTH, GAME_HEIGHT, window);
+                }
+                else if (mouseX >= 66 && mouseX <= 635 && mouseY >= 526 && mouseY <= 669) {
+                    renderInstructions(GAME_WIDTH, GAME_HEIGHT, window);
+                }
+                else if (mouseX >= 66 && mouseX <= 635 && mouseY >= 695 && mouseY <= 839) {
+                    renderLeaderboard(GAME_WIDTH, GAME_HEIGHT, window);
+                }
+            }
+        }
+
+        window.clear();
+        window.draw(home);
+        window.display();
+    }
+}
+void renderLeaderboard(int GAME_WIDTH,
+    int GAME_HEIGHT,
+    RenderWindow& window) {
+    Texture instructionsTexture;
+    instructionsTexture.loadFromFile("./Images/leaderboard.png");
+    Sprite instructions;
+    instructions.setTexture(instructionsTexture);
+    instructions.setPosition(0, 0);
+    while (window.isOpen()) {
+        Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == Event::Closed) {
+                window.close();
+            }
+            else if (event.type == Event::MouseButtonPressed && event.mouseButton.button == Mouse::Left) {
+                int mouseX = event.mouseButton.x;
+                int mouseY = event.mouseButton.y;
+
+                // Check if mouse click falls within the specified area
+                if (mouseX >= 927 && mouseX <= 1277 && mouseY >= 810 && mouseY <= 889) {
+                    // Call renderGame function
+                    renderHome(GAME_WIDTH, GAME_HEIGHT, window);
+                }
+            }
+
+        }
+
+        window.clear();
+        window.draw(instructions);
+        window.display();
+    }
+}
+void renderInstructions(int GAME_WIDTH,
+    int GAME_HEIGHT,
+    RenderWindow& window) {
+    Texture instructionsTexture;
+    instructionsTexture.loadFromFile("./Images/instructions.png");
+    Sprite instructions;
+    instructions.setTexture(instructionsTexture);
+    instructions.setPosition(0, 0);
+    while (window.isOpen()) {
+        Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == Event::Closed) {
+                window.close();
+            }
+            else if (event.type == Event::MouseButtonPressed && event.mouseButton.button == Mouse::Left) {
+                int mouseX = event.mouseButton.x;
+                int mouseY = event.mouseButton.y;
+
+                // Check if mouse click falls within the specified area
+                if (mouseX >= 927 && mouseX <= 1277 && mouseY >= 810 && mouseY <= 889) {
+                    // Call renderGame function
+                    renderHome(GAME_WIDTH, GAME_HEIGHT, window);
+                }
+            }
+            
+        }
+
+        window.clear();
+        window.draw(instructions);
+        window.display();
+    }
+}
 int main()
 {
     srand(time(0));
@@ -382,6 +498,7 @@ int main()
         return 1;
     }
     window.setIcon(32, 32, icon.getPixelsPtr());
-    renderGame(GAME_WIDTH,GAME_HEIGHT,window);
-  
+    renderHome(GAME_WIDTH, GAME_HEIGHT, window);
+
+    /*renderGame(GAME_WIDTH,GAME_HEIGHT,window);*/
 }
